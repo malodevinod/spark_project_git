736d9c224e289204ff18ce7772d5db0b416b8575
vinod malode <malodevinod9108@gmail.com>
2018-12-17 11:08:43 +0530

    addition

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator
+{
+int x=10,y=20;
+	public void add()
+	{
+	System.out.println(x+y);	
+	}
+	public static void main(String args[])
+	{
+	Calculator c=new Calculator();
+	c.add();
+	}
+}
